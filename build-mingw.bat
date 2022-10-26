f63cc5765eaba7a76a5efa187744de7302cfc62f
pigeatgarlic <huyhoangdo0205@gmail.com>
2022-10-26 14:47:26 +0700

    add compile config

--- build-mingw.bat
+++ build-mingw.bat
@@ -1,3 +1,5 @@
-rmdir /Q /S build && mkdir build & cd build && cmake .. -G "Ninja" && cd ..
+set BUILD_CONFIG=%1
+
+rmdir /Q /S build && mkdir build & cd build && cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% && cd ..
 cd build && ninja
 cd ..
--- build-msvc.bat
+++ build-msvc.bat
@@ -1,3 +1,5 @@
-rmdir /Q /S build && mkdir build & cd build && cmake .. -G "Visual Studio 17 2022" && cd ..
+set BUILD_CONFIG=%1
+
+rmdir /Q /S build && mkdir build & cd build && cmake .. -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% -G "Visual Studio 17 2022" && cd ..
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 msbuild build/ALL_BUILD.vcxproj
